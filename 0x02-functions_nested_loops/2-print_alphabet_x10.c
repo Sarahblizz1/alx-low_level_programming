34fb9db84257acf74fb204619debe72822f0db55
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 14:49:57 -0700

    free

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
