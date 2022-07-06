e29ece7dd4fa0ced1fa4c2103e539cbce8fffa84
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 20:35:30 -0700

    fud

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,27 +14,27 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n);
-			if (n != 98)
+			if (n != 98)
 			{
 				printf(", ");
 			}
 			n++;
 		}
 	}
-	else if (n > 98)
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n >= 98)
 		{
-			printf("%d", n);
-			if (n != 98)
+			printf("%d", n);
+			if (n != 98)
 			{
-				printf(", ");				                       }
+				printf(", ");			                       }
 			n--;
 		}
 	}
 	else
 	{
-		printf("98");
+		printf("98);
 	}
 	printf("\n");
 }
