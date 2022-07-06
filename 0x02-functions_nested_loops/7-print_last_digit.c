9679168bfac0ebd68e6fff212070ba133ad52439
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 18:29:54 -0700

    free

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @nld: result of ld
+ * Return: value of ld
+ */
+int print_last_digit(int nld)
+{
+	int pld;
+	pld = (nld % 10);
+
+	if (pld < 0);
+	{
+		pld = (-1 * pld);
+	}
+	_purchar(pld + '0');
+	return (pld);
+}
+
