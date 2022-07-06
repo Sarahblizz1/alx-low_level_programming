7eee25849b9bed6038af1bb7d50305008a256456
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 19:01:57 -0700

    dul

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * jack_bauer -prints minutes
- *
+ * h = hour, m = minutes
  * Return: 24 hours clock line by line
  */
 
 void jack_bauer(void)
 {
-	int h, m:
+	int h, m;
 	
 	for (h = 0; h < 24; h++)
 	{
@@ -16,9 +16,9 @@ void jack_bauer(void)
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
-		        _putchar((m / 10) + '0');
-		        _putchar((m % 10) + '0'); 
-		        _putchar('\n');
+			_putchar((m / 10) + '0');
+		        _putchar((m % 10) + '0');
+			_putchar('\n');
 		 }
 	}
 }
