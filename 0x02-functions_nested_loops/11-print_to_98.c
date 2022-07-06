7c6812344088a405270f3001d8d0e05a0fe633e9
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 20:16:35 -0700

    fool

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: input numbers
+ * Return: Always 0 (success)
+ */
+void print_to_98(int n) 
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);			                               if (n != 98)				                              {
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");				                       }
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
