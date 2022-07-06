6933c521eb308725ad9d1a6566c391e0f1c5806e
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 20:20:46 -0700

    send

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,11 +5,12 @@
  * @n: input numbers
  * Return: Always 0 (success)
  */
-void print_to_98(int n) 
+
+void print_to_98(int n)
 {
-	if (n < 98)
+	if (n < 98)
 	{
-		while (n <= 98)
+		while (n <= 98)
 		{
 			printf("%d", n);			                               if (n != 98)				                              {
 				printf(", ");
