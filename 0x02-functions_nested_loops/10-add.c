8d7d684bc6311d33f17418be82d0e35b97fcae21
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 19:43:14 -0700

    free

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two numbers
+ * @one: first digit
+ * @two: second digit
+ * Return: one + two
+ */
+
+int add (int one,int two)
+{
+	return (one + two);
+}
