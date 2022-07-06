5e970acc58c686de1aa21a6a048bd1b8f25f6917
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 19:08:37 -0700

    fool

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@
 void jack_bauer(void)
 {
 	int h, m;
-	
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
@@ -18,7 +18,9 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((m / 10) + '0');
 		        _putchar((m % 10) + '0');
+
 			_putchar('\n');
+
 		 }
 	}
 }
