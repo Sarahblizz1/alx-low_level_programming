6320aa0925204adaf7712c2acad44fd778104de7
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-05 17:11:23 -0700

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,19 @@
  *
  * Return: x10 a-z
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char alpha, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar (alpha);
+		}
+		co++;
+		_putchar ('\n');
+	}
 }
