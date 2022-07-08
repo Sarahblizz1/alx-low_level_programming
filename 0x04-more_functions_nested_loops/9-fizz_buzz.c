063888661cd4a25fb7a0f34028abfb9302449f70
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-08 16:30:35 -0700

    push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,32 +10,32 @@ int main(void)
 {
 int i = 1;
 
-while (i <= 100)
-{
-	if (i % 3 == 0 && i % 5 == 0)
+	while (i <= 100)
 	{
-		printf("FizzBuzz");
-	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-		printf("Buzz");
-	}
-	else
-	{
-		printf("%i", i);
-	}
-	if (i != 10)
-	{
-		putchar(' ');
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 10)
+		{
+			putchar(' ');
 
-	}
+		}
 
-	i++;
-}
-putchar('\n');
-return (0);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
