0478e6882127433ae8fef914800b273440561509
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-08 18:04:13 -0700

    send

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,14 @@ void more_numbers(void)
 {
 	int i, ro;
 
-		for (ro = 0; ro < 10; ro++)
-		{
+	for (ro = 0; ro < 10; ro++)
+	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
+				_putchar((i / 10) + '0');
+			}
 			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
