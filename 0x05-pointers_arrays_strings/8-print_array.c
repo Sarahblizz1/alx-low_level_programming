ea093ca3ada910e5425a87b2bddd1c8dd20b01e8
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-12 15:17:15 -0700

    push

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "msin.h"
+#include <stdio.h>
+
+/**
+ * print_array - print inputed number
+ * @a: array
+ * @n: number of element
+ */
+
+void point_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
