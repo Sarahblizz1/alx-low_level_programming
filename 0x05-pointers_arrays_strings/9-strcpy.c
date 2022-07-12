006696731e79dab3c47be895ad534f4e660174a2
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-12 16:43:46 -0700

    push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - xopies a string pointed to by @src, a terminating null byte which
+ * is pointed by @dest
+ * @dest: a buffer to copy string to
+ * @src: a source string to copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = o;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
