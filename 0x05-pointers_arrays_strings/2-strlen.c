0d45705b8997fe5c35f8320261cc09000b1e6ff0
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-12 05:46:50 -0700

    push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the lenght of a string
+ * @str: string
+ *
+ * Return: Lenght
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
