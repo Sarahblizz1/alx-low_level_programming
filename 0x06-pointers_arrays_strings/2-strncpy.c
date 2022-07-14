874f7165c9e6854570d1e0b73150a433a95e9e3d
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-14 12:00:19 -0700

    push

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,6 @@
  * Return: A pointer to the resulting string dest.
  */
 
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
@@ -20,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index++];
 
-	for (index = src_len; index <= n; index++)
+	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
