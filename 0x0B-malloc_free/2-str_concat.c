f5ac82762c70a0f1630cbd73ac1541f4f165c9a3
Sarahblizz1 <blizzsarah@gmail.com>
2022-07-25 10:10:04 -0700

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,10 +22,10 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 
-	strout = malloc(sizeof(char) * (i + j + 1));
-
 	for (j = 0; s2[j] != '\0'; j++)
 		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
 	{
